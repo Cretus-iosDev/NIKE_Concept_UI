e9919003ea6ede4cf4bfc091600ecb7720704d1f
Rutik Maraskolhe <84556881+Cretus-iosDev@users.noreply.github.com>
2021-07-14 22:25:30 +0530

    Add files via upload

---
+++ NIKE_Concept_UI/NIKE_Concept_UI/NIKE_Concept_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NIKE_Concept_UIApp.swift
+//  NIKE_Concept_UI
+//
+//  Created by rutik maraskolhe on 14/07/21.
+//
+
+import SwiftUI
+
+@main
+struct NIKE_Concept_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
